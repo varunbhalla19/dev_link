ea2bd274434f1e03c8684ebd3fb500b5909b9ff0
varunbhalla19 <varunbhalla19@gmail.com>
2020-09-14 13:27:34 +0530

    The Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log("listening at ", port);
+});
