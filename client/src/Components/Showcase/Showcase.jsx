d09aee5a8f097e79b26779e055fa0be1044dfa80
varunbhalla19 <varunbhalla19@gmail.com>
2020-09-16 13:32:05 +0530

    Initial Layout

---
+++ client/src/Components/Showcase/Showcase.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => (
+  <section className="landing">
+    <div className="dark-overlay">
+      <div className="landing-inner">
+        <h1 className="x-large">DevLink</h1>
+        <p className="lead">
+          Create a developer profile, share ideas, projects and connect with 
+          other developers.
+        </p>
+        <div className="buttons">
+          <a href="register.html" className="btn btn-primary">
+            Sign Up
+          </a>
+          <a href="login.html" className="btn btn-light">
+            Login
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Navbar;
